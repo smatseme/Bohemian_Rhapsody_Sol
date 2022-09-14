229dc729402e233df3b0b16f35af115f78d5f454
Sipho Matseme <smatseme@gmail.com>
2022-09-14 19:16:39 +0200

    Add project files.

---
+++ Bohemian_API/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bohemian_API.Models
+{
+    public class Album
+    {
+        [Key]
+        public int Album_Id { get; set; }
+        public string Album_Title { get; set; }
+
+    }
+}
---
+++ Bohemian_API/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bohemian_API.Models
+{
+    public class Artist
+    {
+        [Key]
+        public int Artist_Id { get; set; }
+        public string Artist_Name { get; set; }
+    }
+}
